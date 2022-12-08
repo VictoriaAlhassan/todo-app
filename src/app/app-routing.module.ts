48ff4a623ab84d514f3876bb1f4b7121ae826057
Victoria <victoriasoteria@gmail.com>
2022-12-08 09:53:24 -0800

    Tries to add routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,13 @@ import { AddItemComponent } from './add-item/add-item.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import { DetailsComponent } from './details/details.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'app-component', component: AppComponent},
+  {path:'details-component', component: DetailsComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 
-import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
+import { NgModule} from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import {HttpClientModule} from '@angular/common/http';
 import { NgxPaginationModule } from 'ngx-pagination';
@@ -7,27 +7,34 @@ import {FormsModule} from '@angular/forms';
 
 
 import { AppRoutingModule } from './app-routing.module';
+import {RouterModule, Routes} from '@angular/router';
 import { AppComponent } from './app.component';
 import { ItemComponent } from './item/item.component';
-import { AddItemComponent } from './add-item/add-item.component';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import { DetailsComponent } from './details/details.component';
 
+
+ const routes: Routes = [
+  {path: 'home', component: AppComponent},
+  {path:'details', component: DetailsComponent}
+  // {path: '', redirectTo: 'home', pathMatch: 'full'},
+ ]
 
 @NgModule({
   declarations: [
     AppComponent,
     ItemComponent,
-    AddItemComponent,
     DetailsComponent
   ],
   imports: [
     BrowserModule,
+    RouterModule.forRoot(routes),
     AppRoutingModule,
     HttpClientModule,
     NgxPaginationModule,
     NgbModule,
     FormsModule,
+  
     
   ],
   providers: [],
